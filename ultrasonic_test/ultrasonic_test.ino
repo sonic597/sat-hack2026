16406f60c8159ec4bd2e41128853a255297c7646
SneakySquid <96375838+Sn3akySqu1d@users.noreply.github.com>
2026-02-28 11:33:08 +0000

    Created ultrasonic_test.ino

---
+++ ultrasonic_test/ultrasonic_test.ino
@@ -0,0 +1,23 @@
+const int trigPin = A4;
+const int echoPin = A5;
+
+float duration, distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.println(distance);
+  delay(100);
+}
